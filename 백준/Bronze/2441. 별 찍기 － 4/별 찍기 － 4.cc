812220765e183beee171871880f785bb403c08fe
yedam823 <damye823@gmail.com>
2025-06-29 01:03:41 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,17 @@
+#pragma warning(disable: 4996)
+#include<stdio.h>
+#include<stdlib.h>
+
+int main() {
+	int n;
+	scanf("%d", &n);
+	for (int i = n; i > 0; i--) {
+		for (int j = 0; j < n-i; j++) {
+			printf(" ");
+		}
+		for (int j = 0; j < i; j++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}

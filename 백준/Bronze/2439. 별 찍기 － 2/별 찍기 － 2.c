0806fd6e55ecb79107a6242e00c9715a9540777c
yedam823 <damye823@gmail.com>
2025-09-11 18:50:07 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		for (int k = 0; k < n - i; k++)
+		{
+			printf(" ");
+		}
+		for (int j = 0; j < i; j++)
+		{
+			printf("*");
+			
+		}
+		printf("\n");
+		
+	}
+}

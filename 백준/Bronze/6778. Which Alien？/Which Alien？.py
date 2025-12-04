61e69613bc20dc361d2649eabd9bbfee8057c700
김예담 <damye823@gmail.com>
2025-12-04 20:21:49 +0900

    [Bronze IV] Title: Which Alien?, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/6778. Which Alien？/Which Alien？.py
@@ -0,0 +1,8 @@
+i = int(input())
+eye = int(input())
+if i >= 3 and eye <= 4:
+    print('TroyMartian')
+if i <= 6 and eye >= 2:
+    print('VladSaturnian')
+if i <= 2 and eye<= 3:
+    print('GraemeMercurian')
